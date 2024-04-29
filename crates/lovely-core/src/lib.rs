c1aa8c4b8fc603b620b360cd6bbfef8716332a2f
tetraminus <54343976+tetraminus@users.noreply.github.com>
2024-04-28 22:46:44 -0700

    Fix hard crash when printing nil value, change mod path for mac systems

--- crates/lovely-core/src/lib.rs
+++ crates/lovely-core/src/lib.rs
@@ -43,8 +43,11 @@ impl Lovely {
 
         let args = std::env::args().skip(1).collect::<Vec<_>>();
         let mut opts = Options::new(args.iter().map(String::as_str));
-    
+        #[cfg(target_os = "windows")]
         let mut mod_dir = dirs::config_dir().unwrap().join("Balatro\\Mods");
+        #[cfg(target_os = "macos")]
+        let mut mod_dir = dirs::config_dir().unwrap().join("Balatro/Mods");
+        
         let mut is_vanilla = false;
     
         while let Some(opt) = opts.next_arg().expect("Failed to parse argument.") {
