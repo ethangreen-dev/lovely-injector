48d15c78ce5335ba22a5354f0d68615536fc98ed
Ethan Green <ethangreen.dev@gmail.com>
2024-07-02 23:21:29 -0400

    fix: build failure caused by isize, itertools deps

--- crates/lovely-win/src/lib.rs
+++ crates/lovely-win/src/lib.rs
@@ -6,6 +6,7 @@ use lovely_core::log::*;
 use lovely_core::Lovely;
 use lovely_core::sys::LuaState;
 
+use itertools::Itertools;
 use once_cell::sync::OnceCell;
 use retour::static_detour;
 use widestring::U16CString;
@@ -32,7 +33,7 @@ unsafe extern "system" fn DllMain(_: HINSTANCE, reason: u32, _: *const c_void) -
     if reason != 1 {
         return 1;
     }
- 
+
     panic::set_hook(Box::new(|x| unsafe {
         let message = format!("lovely-injector has crashed: \n{x}");
         error!("{message}");
@@ -47,21 +48,21 @@ unsafe extern "system" fn DllMain(_: HINSTANCE, reason: u32, _: *const c_void) -
     }));
 
     let args = env::args().collect_vec();
-    if !args.contains(&"--disable-console".to_string()) { 
+    if !args.contains(&"--disable-console".to_string()) {
         let _ = AllocConsole();
     }
 
     // Initialize the lovely runtime.
     let rt = Lovely::init(&|a, b, c, d| LuaLoadbuffer_Detour.call(a, b, c, d));
     RUNTIME.set(rt).unwrap_or_else(|_| panic!("Failed to instantiate runtime."));
-    
+
     // Quick and easy hook injection. Load the lua51.dll module at runtime, determine the address of the luaL_loadbuffer fn, hook it.
     let handle = LoadLibraryW(w!("lua51.dll")).unwrap();
     let proc = GetProcAddress(handle, s!("luaL_loadbuffer")).unwrap();
     let fn_target = std::mem::transmute::<_, unsafe extern "C" fn(*mut c_void, *const u8, isize, *const u8) -> u32>(proc);
 
     LuaLoadbuffer_Detour.initialize(
-        fn_target, 
+        fn_target,
         |a, b, c, d| lua_loadbuffer_detour(a, b, c, d)
     )
     .unwrap()
