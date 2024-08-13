27ea5096a62df2179f0ea8e97ee318ff056ce4e6
flakywanderer <flakywanderer@gmail.com>
2024-08-13 11:57:23 -0400

    Wrap the pcall better

--- crates/lovely-core/src/patch/module.rs
+++ crates/lovely-core/src/patch/module.rs
@@ -1,6 +1,7 @@
-use std::{ffi::CString, fs, path::PathBuf, ptr};
+use std::{ffi::CString, fs, path::PathBuf, ptr, slice};
+
 use crate::sys::{self, LuaState};
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ModulePatch {
@@ -11,15 +12,15 @@ pub struct ModulePatch {
 
 impl ModulePatch {
     /// Apply a module patch by loading the input file(s) into memory, calling lual_loadbuffer
-    /// on them, and then injecting them into the global `package.loaded` table.
-    /// 
+    /// on them, and then injecting them into the global `package.preload` table.
+    ///
     /// # Safety
     /// This function is unsafe as it interfaces directly with a series of dynamically loaded
     /// native lua functions.
     pub unsafe fn apply<F: Fn(*mut LuaState, *const u8, isize, *const u8, *const u8) -> u32>(
-        &self, 
-        file_name: &str, 
-        state: *mut LuaState, 
+        &self,
+        file_name: &str,
+        state: *mut LuaState,
         lual_loadbuffer: &F,
     ) -> bool {
         // Stop if we're not at the correct insertion point.
@@ -31,32 +32,59 @@ impl ModulePatch {
         let source = fs::read_to_string(&self.source)
             .unwrap_or_else(|e| panic!("Failed to read patch file at {:?}: {e:?}", &self.source));
 
-
         let buf_cstr = CString::new(source.as_str()).unwrap();
         let buf_len = buf_cstr.as_bytes().len();
 
         let name = format!("@{file_name}");
         let name_cstr = CString::new(name).unwrap();
 
-        // Push the global package.loaded table onto the top of the stack, saving its index.
+        // Push the global package.preload table onto the top of the stack, saving its index.
         let stack_top = sys::lua_gettop(state);
         sys::lua_getfield(state, sys::LUA_GLOBALSINDEX, b"package\0".as_ptr() as _);
-        sys::lua_getfield(state, -1, b"loaded\0".as_ptr() as _);
+        sys::lua_getfield(state, -1, b"preload\0".as_ptr() as _);
 
-        // This is the index of the `package.loaded` table.
+        // This is the index of the `package.preload` table.
         let field_index = sys::lua_gettop(state);
 
         // Load the buffer and execute it via lua_pcall, pushing the result to the top of the stack.
-        lual_loadbuffer(state, buf_cstr.into_raw() as _, buf_len as _, name_cstr.into_raw() as _, ptr::null());
+        lual_loadbuffer(
+            state,
+            buf_cstr.into_raw() as _,
+            buf_len as _,
+            name_cstr.into_raw() as _,
+            ptr::null(),
+        );
 
-        sys::lua_pcall(state, 0, -1, 0);
+        let return_code = sys::lua_pcall(state, 0, -1, 0);
 
-        // Insert pcall results onto the package.loaded global table.
-        let module_cstr = CString::new(self.name.clone()).unwrap();
+        if return_code == 0 {
+            // Call succeeded, insert results onto the package.preload global table.
+            let module_cstr = CString::new(self.name.clone()).unwrap();
 
-        sys::lua_setfield(state, field_index, module_cstr.into_raw() as _);
-        sys::lua_settop(state, stack_top);
+            sys::lua_setfield(state, field_index, module_cstr.into_raw() as _);
+            sys::lua_settop(state, stack_top);
 
-        true
+            true
+        } else {
+            // We might quietly convert a number to a string, but this is fine,
+            // as we're just printing it as a diagnostic, and popping it off
+            // immediately afterwards
+            let mut error_cstr_len = 0;
+            let error_cstr = sys::lua_tolstring(state, field_index, &mut error_cstr_len);
+            if !error_cstr.is_null() {
+                log::error!(
+                    "Loading module {} failed with error {}",
+                    self.name,
+                    String::from_utf8_lossy(slice::from_raw_parts(
+                        error_cstr as *const u8,
+                        error_cstr_len.try_into().unwrap()
+                    ))
+                );
+            } else {
+                log::error!("Loading module {} failed with unknown error", self.name);
+            }
+            sys::lua_settop(state, stack_top);
+            false
+        }
     }
 }
