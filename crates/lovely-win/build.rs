4c0c9c13aa2767968a981b0ab29dd7a545f48a4f
lmaple0 <67529520+lmaple0@users.noreply.github.com>
2025-01-28 17:00:00 +0800

    Update build.rs for users whose system disk is not C
    https://github.com/ethangreen-dev/lovely-injector/issues/131

--- crates/lovely-win/build.rs
+++ crates/lovely-win/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let windows_path = "C:\\Windows\\System32\\version.dll";
+    let windows_path = "%SystemDrive%\\Windows\\System32\\version.dll";
     if let Some(path) = std::option_env!("LOVELY_VERSION_PATH") {
         forward_dll::forward_dll_with_dev_path(windows_path, path).unwrap();
     } else {
