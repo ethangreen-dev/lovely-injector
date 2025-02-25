cf11bcaa271093ea8bf8972a151b17d34322df62
Alex Bolduc <axbolduc@gmail.com>
2025-02-24 19:43:06 -0500

    use libloading for dll call

--- crates/lovely-win/src/util/mod.rs
+++ crates/lovely-win/src/util/mod.rs
@@ -1,34 +1,29 @@
+use libc::c_char;
+use libloading::{Library, Symbol};
 use std::ffi::CStr;
-use std::num::ParseIntError;
 use std::str;
+use std::num::ParseIntError;
+use once_cell::sync::Lazy;
 
-use windows::core::s;
-use windows::core::w;
-use windows::Win32::System::LibraryLoader::GetProcAddress;
-use windows::Win32::System::LibraryLoader::LoadLibraryW;
+pub static LUA_LIB: Lazy<Library> = Lazy::new(|| unsafe { Library::new("love.dll").unwrap() });
+
+pub static LOVE_VERSION: Lazy<Symbol<unsafe extern "C" fn() -> *const c_char>> =
+    Lazy::new(|| unsafe { LUA_LIB.get(b"love_version").unwrap() });
 
 // Calls the love_version function from the love.dll
 pub unsafe fn load_version() -> Result<u32, ParseIntError> {
-    let handle = LoadLibraryW(w!("love.dll")).unwrap_or_else(|err| panic!("Could not find dll"));
-
-    let proc = GetProcAddress(handle, s!("love_version"));
-    let fn_target = std::mem::transmute::<
-        _,
-        unsafe extern "C" fn() -> *const i8
-    >(proc);
-
-    let version = fn_target();
-    let version_str = CStr::from_ptr(version).to_str().unwrap_or_else(|err| panic!("Failed to convert love_version result into a str"));
+    let version_result = LOVE_VERSION();
+    let version_str = CStr::from_ptr(version_result).to_str().unwrap_or_else(|err| panic!("Failed to convert love_version result into a str"));
 
     parse_version(version_str)
 }
 
 // Takes the version string "major.minor" and turns it into a u32 where the first 16 bits are the major version and the last 16 are the minor
-fn parse_version(version: &str) -> Result<u32, ParseIntError>{
+fn parse_version(version: &str) -> Result<u32, ParseIntError> {
     let parts: Vec<&str> = version.split('.').collect();
 
     let major = parts[0].parse::<u32>()?;
     let minor = parts[1].parse::<u32>()?;
-    
+
     Ok((major << 16) | (minor & 0xFFFF))
 }
