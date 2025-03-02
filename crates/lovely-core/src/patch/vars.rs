90a0718861a5d3b9bae09450918de430c17a9439
IzawGithub <IzawGithub@pm.me>
2025-03-02 13:20:28 +0100

    Remove use of `once_cell` dependency.

--- crates/lovely-core/src/patch/vars.rs
+++ crates/lovely-core/src/patch/vars.rs
@@ -1,13 +1,12 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::LazyLock};
 
-use once_cell::sync::Lazy;
 use regex_lite::{Regex, Captures};
 
 /// Apply valid var interpolations to the provided line.
 /// Interpolation targets are of form {{lovely:VAR_NAME}}.
 pub fn apply_var_interp(line: &mut String, vars: &HashMap<String, String>) {
     // Cache the compiled regex.
-    let re: Lazy<Regex> = Lazy::new(|| Regex::new(r"\{\{lovely:(\w+)\}\}").unwrap());
+    let re: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\{\{lovely:(\w+)\}\}").unwrap());
 
     let line_replaced = re.replace_all(line, |captures: &Captures| {
         let (_, [var]) = captures.extract();
--- crates/lovely-unix/src/lib.rs
+++ crates/lovely-unix/src/lib.rs
@@ -1,16 +1,16 @@
 use lovely_core::sys::{LuaState, LUA_LIB};
+use std::sync::{LazyLock, OnceLock};
 use std::{env, ptr::null};
 use std::panic;
 use lovely_core::log::*;
 
 use lovely_core::Lovely;
-use once_cell::sync::{Lazy, OnceCell};
 
-static RUNTIME: OnceCell<Lovely> = OnceCell::new();
+static RUNTIME: OnceLock<Lovely> = OnceLock::new();
 
-static RECALL: Lazy<
+static RECALL: LazyLock<
     unsafe extern "C" fn(*mut LuaState, *const u8, isize, *const u8, *const u8) -> u32,
-> = Lazy::new(|| unsafe { *LUA_LIB.get(b"luaL_loadbufferx").unwrap() });
+> = LazyLock::new(|| unsafe { *LUA_LIB.get(b"luaL_loadbufferx").unwrap() });
 
 #[no_mangle]
 #[allow(non_snake_case)]
@@ -20,7 +20,7 @@ unsafe extern "C" fn luaL_loadbuffer(
     size: isize,
     name_ptr: *const u8,
 ) -> u32 {
-    let rt = RUNTIME.get_unchecked();
+    let rt = RUNTIME.get().unwrap_unchecked();
     rt.apply_buffer_patches(state, buf_ptr, size, name_ptr, null())
 }
 
@@ -33,7 +33,7 @@ unsafe extern "C" fn luaL_loadbufferx(
     name_ptr: *const u8,
     mode_ptr: *const u8,
 ) -> u32 {
-    let rt = RUNTIME.get_unchecked();
+    let rt = RUNTIME.get().unwrap_unchecked();
     rt.apply_buffer_patches(state, buf_ptr, size, name_ptr, mode_ptr)
 }
 
