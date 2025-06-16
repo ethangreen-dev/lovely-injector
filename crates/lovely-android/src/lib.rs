7f8902106d69fc7ff39ecaa37993470fc8dc7b56
kodenamekrak <kyelodge2005@gmail.com>
2025-06-16 15:27:04 +0100

    Use snake case

--- crates/lovely-android/src/lib.rs
+++ crates/lovely-android/src/lib.rs
@@ -56,17 +56,16 @@ unsafe extern "C" fn luaL_loadbuffer(
     rt.apply_buffer_patches(state, buf_ptr, size, name_ptr, std::ptr::null())
 }
 
-#[allow(non_snake_case)]
 unsafe fn get_external_files_dir(env: &mut JNIEnv) -> PathBuf {
-    let activityThreadClass = env.find_class("android/app/ActivityThread").unwrap();
-    let contextClass = env.find_class("android/content/Context").unwrap();
-    let externalFilesDirMethod = env.get_method_id(contextClass, "getExternalFilesDir", "(Ljava/lang/String;)Ljava/io/File;").unwrap();
+    let activity_thread_class = env.find_class("android/app/ActivityThread").unwrap();
+    let context_class = env.find_class("android/content/Context").unwrap();
+    let external_files_dir_method = env.get_method_id(context_class, "getExternalFilesDir", "(Ljava/lang/String;)Ljava/io/File;").unwrap();
 
-    let activityThread = env.call_static_method(activityThreadClass, "currentActivityThread", "()Landroid/app/ActivityThread;", &[]).unwrap().l().unwrap();
-    let context = env.call_method(activityThread, "getApplication", "()Landroid/app/Application;", &[]).unwrap().l().unwrap();
-    let externalFilesDir = env.call_method_unchecked(context, externalFilesDirMethod, jni::signature::ReturnType::Object, &[jvalue{l: std::ptr::null_mut()}]).unwrap().l().unwrap();
-    let externFilesDirString: JString = env.call_method(externalFilesDir, "getAbsolutePath", "()Ljava/lang/String;", &[]).unwrap().l().unwrap().into();
-    let utf8 = env.get_string(&externFilesDirString).unwrap();
+    let activity_thread = env.call_static_method(activity_thread_class, "currentActivityThread", "()Landroid/app/ActivityThread;", &[]).unwrap().l().unwrap();
+    let context = env.call_method(activity_thread, "getApplication", "()Landroid/app/Application;", &[]).unwrap().l().unwrap();
+    let external_files_dir = env.call_method_unchecked(context, external_files_dir_method, jni::signature::ReturnType::Object, &[jvalue{l: std::ptr::null_mut()}]).unwrap().l().unwrap();
+    let external_files_dir_string: JString = env.call_method(external_files_dir, "getAbsolutePath", "()Ljava/lang/String;", &[]).unwrap().l().unwrap().into();
+    let utf8 = env.get_string(&external_files_dir_string).unwrap();
     PathBuf::from(utf8.to_str().unwrap())
 }
 
