936e888d51fcbd372e1f009b00f6ad05141a82ca
WilsontheWolf <git@shorty.systems>
2025-10-04 01:21:23 -0600

    fix: new buffer patches (untested)

--- crates/liblovely/lovely.h
+++ crates/liblovely/lovely.h
@@ -1,7 +1,7 @@
 #ifndef LOVELY_H
 #define LOVELY_H
 
-void lovely_init(void *loadbufferx, void *lua_call, void *lua_pcall, void *lua_getfield, void *lua_setfield, void *lua_gettop, void *lua_settop, void *lua_pushvalue, void *lua_pushcclosure, void *lua_tolstring);
+void lovely_init(void *loadbufferx, void *lua_call, void *lua_pcall, void *lua_getfield, void *lua_setfield, void *lua_gettop, void *lua_settop, void *lua_pushvalue, void *lua_pushcclosure, void *lua_tolstring, void *lua_createtable, void *lua_pushboolean, void *lua_pushnumber, void *lua_pushstring, void *lua_settable, void *lua_type, void *luaL_register);
 
 int lovely_apply_patches(lua_State *L, const char *buff, size_t sz,
     const char *name, const char *mode);
