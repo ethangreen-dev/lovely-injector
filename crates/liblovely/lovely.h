97512d2c692ca3e173db5f65d2e9192b5980dbd0
WilsontheWolf <git@shorty.systems>
2025-10-09 23:23:17 -0600

    use struct for liblovely

--- crates/liblovely/lovely.h
+++ crates/liblovely/lovely.h
@@ -1,8 +1,27 @@
 #ifndef LOVELY_H
 #define LOVELY_H
 
-void lovely_init(void *loadbufferx, void *lua_call, void *lua_pcall, void *lua_getfield, void *lua_setfield, void *lua_gettop, void *lua_settop, void *lua_pushvalue, void *lua_pushcclosure, void *lua_tolstring, void *lua_createtable, void *lua_pushboolean, void *lua_pushnumber, void *lua_pushstring, void *lua_settable, void *lua_type, void *luaL_register, void *luaL_checklstring);
+struct LuaLib {
+	void *lua_call;
+	void *lua_pcall;
+	void *lua_getfield;
+	void *lua_setfield;
+	void *lua_gettop;
+	void *lua_settop;
+	void *lua_pushvalue;
+	void *lua_pushcclosure;
+	void *lua_tolstring;
+	void *lua_type;
+	void *luaL_register;
+	void *lua_pushstring;
+	void *lua_pushnumber;
+	void *lua_pushboolean;
+	void *lua_settable;
+	void *lua_createtable;
+	void *luaL_checklstring;
+};
 
-int lovely_apply_patches(lua_State *L, const char *buff, size_t sz,
-    const char *name, const char *mode);
+void lovely_init(void *loadbufferx, struct LuaLib);
+
+int lovely_apply_patches(lua_State *L, const char *buff, size_t sz, const char *name, const char *mode);
 #endif // LOVELY_H
